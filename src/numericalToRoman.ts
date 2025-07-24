dc68b246b001841d2b85efb20a281035f874af14
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-07-24 06:28:59 +0000

    passing code for roman 6

--- src/numericalToRoman.ts
+++ src/numericalToRoman.ts
@@ -1,12 +1,15 @@
 export function numericalToRoman(input: number) {
-    if (input === 5) {
-        return "V";
-    }
-    if (input === 4) {
-        return "IV";
-    }
-    if (input > 0) {
-        return "I".repeat(input);
-    }
-    return ""
+  if (input === 6) {
+    return "VI"
+  }
+  if (input === 5) {
+    return "V";
+  }
+  if (input === 4) {
+    return "IV";
+  }
+  if (input > 0) {
+    return "I".repeat(input);
+  }
+  return ""
 }
