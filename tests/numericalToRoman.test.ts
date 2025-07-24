e665fb92d8b84e8bee78da181b956e2a2696c1d8
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-07-24 06:10:04 +0000

    failing test case for roman 2

--- tests/numericalToRoman.test.ts
+++ tests/numericalToRoman.test.ts
@@ -1,12 +1,16 @@
 import { describe, it, expect } from 'vitest'
 import { numericalToRoman } from '../src/numericalToRoman'
 
-describe("numericalToRoman",()=>{
-  it("should return empty string for 0",()=>{
+describe("numericalToRoman", () => {
+  it("should return empty string for 0", () => {
     expect(numericalToRoman(0)).toBe("")
   })
 
   it("should return I for 1", () => {
     expect(numericalToRoman(1)).toBe("I")
+  })
+
+  it("should return II for 2", () => {
+    expect(numericalToRoman(2)).toBe("II")
   })
 })
