1087357e8083daeec7cc360efb37daee750cd953
Siya Agarwal <sagarwal@kyruus.com>
2025-07-24 11:49:12 +0530

    refactored to remove duplication

--- src/numericalToRoman.ts
+++ src/numericalToRoman.ts
@@ -1,12 +1,6 @@
 export function numericalToRoman(input: number) {
-  if (input === 1) {
-    return "I";
-  }
-  if (input === 2) {
-    return "II"
-  }
-  if(input === 3){
-    return "III"
+  if (input > 0) {
+    return "I".repeat(input);
   }
   return ""
 }
