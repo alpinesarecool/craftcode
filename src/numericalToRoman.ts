81c6cbada0b2a91a50c847fd59e2102d131f611c
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-07-24 06:42:19 +0000

    extract roman constants

--- src/numericalToRoman.ts
+++ src/numericalToRoman.ts
@@ -1,12 +1,15 @@
+const ROMAN_5 = "V"
+const ROMAN_1 = "I"
 export function numericalToRoman(input: number) {
-  if (input>5){
-    return "V"+ repeatI(input%5)
+
+  if (input > 5) {
+    return ROMAN_5 + repeatI(input % 5)
   }
   if (input === 5) {
-    return "V";
+    return ROMAN_5;
   }
   if (input === 4) {
-    return "IV";
+    return ROMAN_1 + ROMAN_5;
   }
   if (input > 0) {
     return repeatI(input);
@@ -15,5 +18,5 @@ export function numericalToRoman(input: number) {
 }
 
 function repeatI(input: number) {
-  return "I".repeat(input);
+  return ROMAN_1.repeat(input);
 }
