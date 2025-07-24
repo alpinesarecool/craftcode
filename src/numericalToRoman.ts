91b3fd538430cec0beb553ec313650f1c3c0c5d5
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-07-24 06:46:07 +0000

    add passing code for roman 9

--- src/numericalToRoman.ts
+++ src/numericalToRoman.ts
@@ -1,7 +1,10 @@
 const ROMAN_5 = "V"
 const ROMAN_1 = "I"
 export function numericalToRoman(input: number) {
-
+  if (input === 9)
+  {
+    return ROMAN_1 + "X"
+  }
   if (input > 5) {
     return ROMAN_5 + repeatI(input % 5)
   }
