3d9dada5849eac935139716b13d3d09ae188a61b
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-07-24 06:12:07 +0000

    make roman 2 pass

--- src/numericalToRoman.ts
+++ src/numericalToRoman.ts
@@ -1,6 +1,9 @@
-export function numericalToRoman(input: number){
-  if(input ===1 ){
+export function numericalToRoman(input: number) {
+  if (input === 1) {
     return "I";
+  }
+  if (input === 2) {
+    return "II"
   }
   return ""
 }
